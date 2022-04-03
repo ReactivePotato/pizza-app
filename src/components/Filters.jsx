ae093586482cc7322ad7d61453a4451d04be7a77
ReactivePotato <potamatto@gmail.com>
2022-04-04 04:43:00 +1000

    added router

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/Filters.scss'
 
 const Filters = () => {
   return (
     <div className='filters'>
-      <div className='filter-button'>фильтр 1</div>
-      <div className='filter-button'>фильтр 2</div>
-      <div className='filter-button'>фильтр 3</div>
-      <div className='filter-button'>фильтр 4</div>
+      
+      <Link to="#"><div className='filter-button'>фильтр 1</div></Link>
+      <Link to="#"><div className='filter-button'>фильтр 2</div></Link>
+      <Link to="#"><div className='filter-button'>фильтр 3</div></Link>
+      <Link to="#"><div className='filter-button'>фильтр 4</div></Link>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import '../styles/Header.scss'
 const Header = () => {
   return (
     <div className='header'>
-      <img src="https://pics.clipartpng.com/midle/Pizza_PNG_Clipart-572.png" alt="pizza" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/Eucalyp-Deus_Pizza.png" alt="pizza" />
       <div className='header-title'>
       <div className='header-title-main'>Такая себе пицца</div>
       <div className='header-title-small'>тут мелкий еле живой текст про пиццу</div>
