21f244deae5bfa58d678046cdf2d499a97be5277
ReactivePotato <potamatto@gmail.com>
2022-04-04 04:18:06 +1000

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <img src="https://pics.clipartpng.com/midle/Pizza_PNG_Clipart-572.png" alt="pizza" />
       <div className='header-title'>
       <div className='header-title-main'>Такая себе пицца</div>
-      <div className='header-title-small'>тут мелкий, еле живой текст про пиццу</div>
+      <div className='header-title-small'>тут мелкий еле живой текст про пиццу</div>
       </div>
       <div><HeaderButtons /></div>
     </div>
