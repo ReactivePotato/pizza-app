d348601a9709f4c77259ca4e2ee1ac406885b208
ReactivePotato <potamatto@gmail.com>
2022-04-10 22:10:43 +1000

    added counter for goods in cart at header

--- src/components/HeaderButtons.jsx
+++ src/components/HeaderButtons.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { useSelector } from 'react-redux';
 
 const HeaderButtons = () => {
+  const currentPizzasAmount = useSelector((state) => state.pizzasInCartReducer)
+
+  const totalPizzasInCart = currentPizzasAmount.reduce((acc, rec) => {
+    return acc + rec.pizza_amount
+  }, 0)
+
   return (
     <Link to="/cart" className='header_buttons'>
       <div className='header_btn_amount'>100 ₽.</div>
       <div className='header_btn_divider'></div>
-      <div className='header_btn_cart'><FontAwesomeIcon icon={faCartShopping} /></div>
+      <div className='header_btn_cart_img'><FontAwesomeIcon icon={faCartShopping} /></div>
+      <div className='header_btn_incart_bg'></div>
+      <div className='header_btn_incart'>{totalPizzasInCart}</div>
     </Link>
   )
 }
