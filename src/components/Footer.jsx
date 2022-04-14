4e34edbb6769e6c5e30676c5b774469582baabe9
ReactivePotato <potamatto@gmail.com>
2022-04-15 01:55:42 +1000

    added footer menu, app global styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons'
 import '../styles/Footer.scss'
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className='footer_left_col'>
+        <ul>
+          <li><Link to="/aboutus">О нас</Link></li>
+          <li><Link to="/faq">Частые вопросы</Link></li>
+          <li><Link to="/terms">Пользовательское соглашение</Link></li>
+          <li><Link to="/policy">Политика конфиденциальности</Link></li>
+        </ul>
+      </div>
+      <div className='footer_mid_col'>
+      <ul>
+          <li><Link to="/franchise">Франшиза</Link></li>
+          <li><Link to="/invest">Инвестиции</Link></li>
+          <li><Link to="/dealers">Поставщикам</Link></li>
+          <li><Link to="/feedback">Предложения</Link></li>
+        </ul>
+      </div>
+      <div className='footer_right_col'>
+      <ul>
+          <li><SocialIcon url="https://instagram.com" style={{width: 30, height: 30 }}/></li>
+          <li><SocialIcon url="https://vk.com"/></li>
+          <li><SocialIcon url="https://whatsapp.com"/></li>
+          <li><SocialIcon url="https://telegram.com"/></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
